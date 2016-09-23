3e5518abc1c5cb49338d619d8f2adb2046680043
JadersonBarreto <jaderson-oliveira@hotmail.com>
2016-09-23 20:45:46 -0300

    Retangulo.cpp
    Correção com adição de comentários necessários para uma boa interpretação do código.

--- Tratamento-de-poligonos/Projeto/Retangulo.cpp
+++ Tratamento-de-poligonos/Projeto/Retangulo.cpp
@@ -1,11 +1,11 @@
 #include "Retangulo.h"
 
-Retangulo::Retangulo(int x,int y,int largura,int altura){
+Retangulo::Retangulo(int x,int y,int largura,int altura){ //Função que adiciona os vertices de um poligono retangulo
     addVertice(x,y);
     addVertice(x,y-altura);
     addVertice(x+largura,y-altura);
     addVertice(x+largura,y);
 }
 
-Retangulo::~Retangulo(){
+Retangulo::~Retangulo(){ // Destrutor da class
 }
